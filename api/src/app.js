1a7a02b0f9e622af01b43e084ed54f1e83561c7a
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2021-03-05 13:56:23 +0200

    Add CORS middleware

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
 const app = express()
 
 const taskRouter = require('./routes/taskRoutes')
@@ -7,6 +8,7 @@ const taskRouter = require('./routes/taskRoutes')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
+app.use(cors())
 
 app.use('/api', taskRouter)
 
