72fd3926d626f33011ad1863790515dddf29e79f
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2021-03-05 13:16:14 +0200

    Remove unnecessary API route

--- api/src/routes/taskRoutes.js
+++ api/src/routes/taskRoutes.js
@@ -3,15 +3,15 @@ const taskController = require('../controllers/taskController')
 
 const router = express.Router()
 
-router.route('/tasks')
+router
+  .route('/tasks')
   .post(taskController.addTask)
   .get(taskController.getAllTasks)
 
-router.route('/tasks/:id')
+router
+  .route('/tasks/:id')
   .put(taskController.updateTask)
   .get(taskController.getTaskById)
   .delete(taskController.deleteTask)
-
-router.get('/tasks/device/:id', taskController.getTasksByDevice)
 
 module.exports = router
