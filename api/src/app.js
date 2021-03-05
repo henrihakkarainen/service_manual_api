094c1bf04400eda9fd1e50128c4f1b10aa2f1a42
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2021-03-05 11:12:28 +0200

    Capitalize constant port

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ app.use(helmet())
 
 app.use('/api', taskRouter)
 
-const port = process.env.port || 3000
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}!`)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`)
 })
