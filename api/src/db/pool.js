4d056c1fbc69db2acf685ea4dc8ebfe39f0d8b4f
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2021-03-06 13:13:34 +0200

    Commenting

--- api/src/db/pool.js
+++ api/src/db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg')
 
-// Init pool for database connections
+// Init pool for database connections (these values should be moved to env variables)
 const pool = new Pool({
   user: 'worker',
   host: 'db',
